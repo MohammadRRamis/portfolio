764b4293d23887ae911a4c25824b6726379232ed
Mohammad Al Ramis <95681437+MohammadAlRamis@users.noreply.github.com>
2022-10-07 22:04:43 +0300

    Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, {useState} from 'react'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 const Navbar = () => {
-  return (
-    <div>
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
-    </div>
+
+  return (
+    <div className='fixed w-full z-[100]'>
+      <div className='flex justify-between items-center w-full h-full px-12 py-6 2xl:px-16'>
+        <Image src="/../public/assets/logo.png" alt="image" width="196" height="73" />
+        <div>
+          <ul className='hidden md:flex'>
+            <Link href='/'>
+              <li className='ml-20 uppercase hover:border-b'>Work</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-20 uppercase'>About</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-20 uppercase'>CV</li>
+            </Link>
+          </ul>
+          <div onClick={handleNav} className='md:hidden'>
+            <AiOutlineMenu size={25}/>
+          </div>
+          </div>
+        </div>
+        <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : 'hidden'}>
+          <div onClick={handleNav} className='flex flex-col w-full  items-end px-12 py-12'>
+            <div>
+              <AiOutlineClose size={25}/>
+            </div>
+            
+          <ul className='flex flex-col items-center justify-center w-full h-screen'>
+            <Link href='/'>
+              <li className='mb-20 uppercase text-[#F8F9FA]'>Work</li>
+            </Link>
+            <Link href='/'>
+              <li className='mb-20 uppercase text-[#F8F9FA]'>About</li>
+            </Link>
+            <Link href='/'>
+              <li className='mb-20 uppercase text-[#F8F9FA]'>CV</li>
+            </Link>
+          </ul>
+          
+          </div>
+
+        </div>
+      </div>
   )
 }
 
