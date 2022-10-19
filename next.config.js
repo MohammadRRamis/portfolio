368cc920b94de669260b9589283e622d18c07f2c
Your Name <you@example.com>
2022-10-19 23:29:16 +0300

    next config js edits

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+};
